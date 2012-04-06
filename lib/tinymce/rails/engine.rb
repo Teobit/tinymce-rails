d2fedf9a735e23446f20638f190f3178f04bf8a2
Mario Visic <mario@mariovisic.com>
2012-04-07 00:43:45 +0800

    Setting the tinyMCE assets to precompile now runs for all groups.

--- lib/tinymce/rails/engine.rb
+++ lib/tinymce/rails/engine.rb
@@ -1,7 +1,7 @@
 module TinyMCE
   module Rails
     class Engine < ::Rails::Engine
-      initializer "configure assets" do |app|
+      initializer "configure assets", :group => :all do |app|
         app.config.assets.precompile << 'tinymce/*'
       end
     end
